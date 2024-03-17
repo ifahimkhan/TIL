de19ca1eee3ff261f7008e6635ff71081345dd11
ifahimkhan <khan.fahim95@hotmail.com>
2024-03-17 23:10:01 +0400

    if applied this commit will add implementation of State and binding object  data flowing from things store to content view via binding to state

--- TIL/ContentView.swift
+++ TIL/ContentView.swift
@@ -1,40 +1,44 @@
 import SwiftUI
 
 struct ThingStore {
-  var things: [String] = []
+    var things: [String] = []
 }
 
 struct ContentView: View {
-  @State private var showAddThing = false
-  let tempThings = ["YOLO", "BTW"]
+    @State private var showAddThing = false
+    @State private var myThings = ThingStore()
 
-  var body: some View {
-    NavigationStack {
-      VStack(spacing: 20) {
-        ForEach(tempThings, id: \.self) { thing in
-          Text(thing)
+    var body: some View {
+        NavigationStack {
+            VStack(spacing: 20) {
+                if myThings.things.isEmpty{
+                    Text("Add acronyms you will learn")
+                        .foregroundColor(.gray)
+                }
+                ForEach(myThings.things, id: \.self) { thing in
+                    Text(thing)
+                }
+                Spacer()
+            }
+            .navigationTitle("TIL")
+            .toolbar {
+                ToolbarItem {
+                    // swiftlint:disable:next multiple_closures_with_trailing_closure
+                    Button(action: { showAddThing.toggle() }) {
+                        Image(systemName: "plus.circle")
+                            .font(.title)
+                    }
+                }
+            }
+            .sheet(isPresented: $showAddThing) {
+                AddThingView( someThings: $myThings)
+            }
         }
-        Spacer()
-      }
-      .navigationTitle("TIL")
-      .toolbar {
-        ToolbarItem {
-          // swiftlint:disable:next multiple_closures_with_trailing_closure
-          Button(action: { showAddThing.toggle() }) {
-            Image(systemName: "plus.circle")
-              .font(.title)
-          }
-        }
-      }
-      .sheet(isPresented: $showAddThing) {
-        AddThingView()
-      }
     }
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        ContentView()
+    }
 }
