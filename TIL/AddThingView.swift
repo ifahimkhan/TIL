c26350b37f0538e9a2e2f406375ef3a6b999724e
ifahimkhan <khan.fahim95@hotmail.com>
2024-03-18 18:55:31 +0400

    if applied this commit will add implementation of StateObject and observableObject and published the object class

--- TIL/AddThingView.swift
+++ TIL/AddThingView.swift
@@ -1,22 +1,32 @@
 import SwiftUI
 
 struct AddThingView: View {
-  @Environment(\.dismiss) var dismiss
-    @Binding var someThings: ThingStore
+    @Environment(\.dismiss) var dismiss
+    @ObservedObject var someThings: ThingStore
+    @State private var thing = ""
+    @FocusState private var thingIsFocused: Bool
 
-  var body: some View {
-    VStack {
-      Button("Done") {
-          someThings.things.append("FOMO")
-        dismiss()
-      }
-      Spacer()
+    var body: some View {
+        VStack {
+            TextField("Things I Learned",text: $thing)
+                .focused($thingIsFocused).textFieldStyle(.roundedBorder)
+                .disableAutocorrection(true)
+                .padding()
+            Button("Done") {
+                if !thing.isEmpty{
+                    someThings.things.append(thing)
+                }
+                dismiss()
+            }
+            Spacer()
+        }.environment(\.textCase, nil).onAppear{
+            thingIsFocused = true
+        }
     }
-  }
 }
 
 struct AddThingView_Previews: PreviewProvider {
-  static var previews: some View {
-      AddThingView(someThings: .constant(ThingStore()))
-  }
+    static var previews: some View {
+        AddThingView(someThings:ThingStore())
+    }
 }
--- TIL/TILApp.swift
+++ TIL/TILApp.swift
@@ -3,9 +3,10 @@ import SwiftUI
 
 @main
 struct TILApp: App {
-  var body: some Scene {
-    WindowGroup {
-      ContentView()
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.textCase, .uppercase)
+        }
     }
-  }
 }
